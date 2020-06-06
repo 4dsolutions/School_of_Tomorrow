eb7e5d718e9e9ac753fd96cfa87003b7a5860ba0
Kirby Urner <kirby.urner@gmail.com>
2020-06-06 08:07:16 -0700

    fixed blender code

--- blender_one.py
+++ blender_one.py
@@ -1,11 +1,14 @@
 import bpy
 from qrays import Qvector
+import sys
 
+sys.path.append("C:\\Users\\Kirby\\School_of_Tomorrow")
 from itertools import permutations
 g = permutations((2,1,1,0))
 
 ORIGIN_IVM = Qvector((0,0,0,0))
-SPOKES_IVM = [Qvector(v) for v in {p for p in g}]  # set comprehension in list comprehension
+# set comprehension in list comprehension
+SPOKES_IVM = [Qvector(v) for v in {p for p in g}] 
 ORIGIN_XYZ = ORIGIN_IVM.xyz().xyz # (0,0,0)
 
 c6xty_ball = bpy.ops.mesh.primitive_ico_sphere_add
@@ -15,4 +18,4 @@ bpy.ops.object.shade_smooth()
 for qv in SPOKES_IVM:
     xyz = qv.xyz().xyz
     c6xty_ball(radius=0.5, enter_editmode=False, location=xyz)
-    bpy.ops.object.shade_smooth()
+    # bpy.ops.object.shade_smooth()
