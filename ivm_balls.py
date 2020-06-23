447f2c6dd59aa83f881dca07f3c7e99d7a978c5b
Kirby Urner <kirby.urner@gmail.com>
2020-06-23 03:23:28 -0700

    improve ball packing algorithm

--- ivm_balls.py
+++ ivm_balls.py
@@ -1,13 +1,13 @@
 import bpy
+
 import sys
-
 sys.path.append("C:\\Users\\Kirby\\School_of_Tomorrow")
+
 from qrays import Qvector
-
-
 from functools import partial
-from itertools import permutations
 
+
+from itertools import permutations
 g = permutations((2,1,1,0))
 
 ORIGIN_IVM = Qvector((0,0,0,0))
@@ -30,10 +30,13 @@ def next_layer(curr_layer, prev_layer):
                 next_layer.add(v_sum)
     return sorted(list(next_layer))
 
-nl   = next_layer(nucleus, nucleus) # 1-freq
-nnl  = next_layer(nl, nucleus)      # 2-freq
-nnnl = next_layer(nnl, nl)          # 3-freq
-nnnnl= next_layer(nnnl, nnl)        # 4-freq
+nl       = next_layer(nucleus, nucleus)   # 1-freq
+nnl      = next_layer(nl, nucleus)        # 2-freq
+nnnl     = next_layer(nnl, nl)            # 3-freq
+nnnnl    = next_layer(nnnl, nnl)          # 4-freq
+nnnnnl   = next_layer(nnnnl, nnnl)        # 5-freq
+nnnnnnl  = next_layer(nnnnnl, nnnnl)      # 6-freq
+nnnnnnnl = next_layer(nnnnnnl, nnnnnl)    # 7-freq
 
 def get_xyz(qvectors):
     xyz_vectors = []
@@ -45,14 +48,15 @@ def get_xyz(qvectors):
 c6xty_ball = partial(bpy.ops.mesh.primitive_ico_sphere_add, subdivisions=1, radius=0.5, enter_editmode=False)
 
 
-for ball in get_xyz(nl):
-    c6xty_ball(location=ball.xyz)
+#for ball in get_xyz(nl):
+#    c6xty_ball(location=ball.xyz)
 
-for ball in get_xyz(nnl):
+#for ball in get_xyz(nnl):
+#    c6xty_ball(location=ball.xyz)
+    
+#for ball in get_xyz(nnnl):
+#    c6xty_ball(location=ball.xyz)
+
+for ball in get_xyz(nnnnnnnl):
     c6xty_ball(location=ball.xyz)
     
-for ball in get_xyz(nnnl):
-    c6xty_ball(location=ball.xyz)
-
-for ball in get_xyz(nnnnl):
-    c6xty_ball(location=ball.xyz)
