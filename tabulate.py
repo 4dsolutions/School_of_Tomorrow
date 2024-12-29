2aa301a7537761032de9cf514c89849ddbb0ea36
Kirby Urner <kirby.urner@gmail.com>
2024-12-28 20:12:06 -0800

    for use in spyder etc

---
+++ tabulate.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 28 20:02:12 2024
+
+Make a pandas table of concentric hierarchy 
+polys using qray coords (A, B, C, D) and 
+xyz coords (X, Y, Z).
+
+@author: kirbyurner
+"""
+
+import flextegrity as fx
+import pandas as pd
+
+arrays = [
+    coordsys := ["IVM", "IVM", "IVM", "IVM", "XYZ", "XYZ", "XYZ"],
+    coordnms := ["A", "B", "C", "D", "X", "Y", "Z"]]
+col_tuples = list(zip(*arrays))
+col_tuples
+
+def tabulate(shape, dec=False, n=8):
+    vs = shape.vertexes.items()
+    shape_name = shape.nick
+    rows = []
+    row_tuples = []
+
+    for label, v in vs:
+        xyz = v.xyz
+        row_tuples.append((shape_name, label))
+        if not dec:
+            rows.append(dict(name = label, 
+                             A=v.a, 
+                             B=v.b, 
+                             C=v.c, 
+                             D = v.d, 
+                             X = xyz.x, 
+                             Y = xyz.y, 
+                             Z = xyz.z))
+        else:
+            rows.append(dict(name = label, 
+                             A = v.a.evalf(n).round(n), 
+                             B = v.b.evalf(n).round(n), 
+                             C = v.c.evalf(n).round(n), 
+                             D = v.d.evalf(n).round(n), 
+                             X = xyz.x.evalf(n).round(n), 
+                             Y = xyz.y.evalf(n).round(n), 
+                             Z = xyz.z.evalf(n).round(n)))  
+                        
+    df = pd.DataFrame.from_dict(rows)
+    col_index = pd.MultiIndex.from_tuples(col_tuples, names=["System", "Coords"])
+    row_index = pd.MultiIndex.from_tuples(row_tuples, names=["Shape", "Verts"])
+    df.index = row_index
+    df.drop(columns="name", axis=1, inplace=True)
+    df.columns = col_index
+    return df
+
+if __name__ == "__main__":
+    tet = tabulate(fx.Tetrahedron())
+    invtet = tabulate(fx.InvTetrahedron())
+    cu   = tabulate(fx.Cube())
+    octa = tabulate(fx.Octahedron())
+    rd   = tabulate(fx.RD())
+    co   = tabulate(fx.Cuboctahedron())
+    
+    master_df = pd.concat((tet, invtet, cu, octa, rd, co), axis=0)
+    
